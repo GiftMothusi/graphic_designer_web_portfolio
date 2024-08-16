a5d93cf9e39a25c65f245b9f69b7a8e77e541e49
Gift <giftgabanakgosi@gmail.com>
2024-08-16 19:49:02 +0200

    fix:added proper typescript extension,also fixed lib utils import , also imported flating nav to pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Testimonials from "@/components/Testimonials";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { navItems } from "@/data";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="w-full h-full">
+        <FloatingNav navItems={navItems}/>
         <Hero/>
         <Testimonials/>
         <Footer/>
