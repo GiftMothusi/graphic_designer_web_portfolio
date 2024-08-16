f40fbe1db5a25c47a24d12f291c4e9c31af2b1bb
Gift <giftgabanakgosi@gmail.com>
2024-08-16 19:43:02 +0200

    added flating nav aceternity ui file

--- src/components/FloatingNav.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FloatingNav = () => {
-  return (
-    <div>FloatingNav</div>
-  )
-}
-
-export default FloatingNav
---
+++ src/data/index.ts
@@ -0,0 +1,5 @@
+export const navItems = [
+    { name: "Home", link: "#about" },
+    { name: "My Work", link: "#projects" },
+    { name: "Contact", link: "#contact" },
+  ];
